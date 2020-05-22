ed2b3265fe6298ee55bcbc01cef39a708ee2e5a5
oliversandli <oliversandli@icloud.com>
2020-05-21 19:39:55 -0500

    Remove unnecessary 'if name main' statement

--- setup.py
+++ setup.py
@@ -4,29 +4,28 @@
 import setuptools
 
 
-if __name__ == "__main__":
-    with open("README.md", "r") as fh:
-        LONG_DESCRIPTION = fh.read()
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
 
-    setuptools.setup(
-        name="class_d-oliversandli",
-        version="0.0.1",
-        author="Oliver Sandli",
-        author_email="racerpingpong@gmail.com",
-        description="Get a license from the GitHub API",
-        long_description=LONG_DESCRIPTION,
-        long_description_content_type="text/markdown",
-        url="https://github.com/oliversandli/class_d",
-        packages=["class_d"],
-        scripts=["bin/class_d"],
-        install_requires=[
-            "inquirer",
-            "requests"
-        ],
-        classifiers=[
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: Unix"
-        ],
-        python_requires='>=3.6'
-    )
+setuptools.setup(
+    name="class_d-oliversandli",
+    version="0.0.1",
+    author="Oliver Sandli",
+    author_email="racerpingpong@gmail.com",
+    description="Get a license from the GitHub API",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/oliversandli/class_d",
+    packages=["class_d"],
+    scripts=["bin/class_d"],
+    install_requires=[
+        "inquirer",
+        "requests"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix"
+    ],
+    python_requires='>=3.6'
+)
